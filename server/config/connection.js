b7aaf6b446a2a6b8a5db6affd709c5270ef3cae8
rileyale001 <alexjtriley@gmail.com>
2021-08-31 15:27:46 -0700

    make it better

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/shopmern', {
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/shopmern', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   useCreateIndex: true,
